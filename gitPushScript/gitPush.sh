2f083c89318455f02f0bf28491bdb0d94c1ab075
parths3an <parth.93mukesh@gmail.com>
2017-01-27 01:15:46 -0800

    Renamed the folder to remove extra space from between

---
+++ gitPushScript/gitPush.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Automate the process of doing git push
+#update the value in the status.txt file
+git status > temp.txt
+if grep -q committed temp.txt; then
+    echo "There are unstaged changes..."
+    git status
+    git add .
+    echo "Added all the files to the stagging area."
+    #Save the commit message in a variable to read
+    echo "Enter the commit message and then hit enter to commit changes"
+    read commitMessage
+    git commit -m "$commitMessage"
+    #git status
+    git push
+    git status > temp.txt
+else
+    echo "Nothing to commit, working directory is clean."
+    fi
+
