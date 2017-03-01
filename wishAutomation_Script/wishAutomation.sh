cd980fa21ad033f2ee0f902ca90b3643a8a7438b
parths3an <parth.93mukesh@gmail.com>
2017-02-28 22:36:28 -0800

    Changed the name of the file wish.txt to wishbirthday to differentiate between birthday and anniversary

--- wishAutomation_Script/wishAutomation.sh
+++ wishAutomation_Script/wishAutomation.sh
@@ -4,7 +4,7 @@
 #Ask user to pass the email address they want to send email.
 #Use the positional parameter to retrive the receiver's email address.
 
-ssmtp $1 < wish.txt  
+ssmtp $1 < wishBirthday.txt  
 
 
 
