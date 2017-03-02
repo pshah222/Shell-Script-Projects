10036c28a96ef97962dea8bc5e2ea6ee04bbf4cc
parths3an <parth.93mukesh@gmail.com>
2017-03-01 18:25:20 -0800

    Added list of email addresses, and trying to use a while loop in .sh file toomatically take the input on more than one email address.

--- wishAutomation_Script/wishAutomation.sh
+++ wishAutomation_Script/wishAutomation.sh
@@ -4,7 +4,7 @@
 #Ask user to pass the email address they want to send email.
 #Use the positional parameter to retrive the receiver's email address.
 
-ssmtp $1 < wishBirthday.txt  
+ssmtp a | emailList.txt < wishBirthday.txt  
 
 
 
