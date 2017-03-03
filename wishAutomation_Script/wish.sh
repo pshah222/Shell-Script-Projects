10af2a59defe7c0fd0d136c2d501a5baf9aca6ff
parths3an <parth.93mukesh@gmail.com>
2017-03-03 01:04:12 -0800

    Final working version of the program; Added the File descriptions in the fileNameExplanation.txt

--- wishAutomation_Script/wish.sh
+++ wishAutomation_Script/wish.sh
@@ -4,16 +4,17 @@
 #Ask user to pass the email address they want to send email.
 #Use the positional parameter to retrive the receiver's email address.
 #save the input file ina variable to use 
-birthdayList=bdaysList.txt
+birthdayList=sendEmailto.txt
 
 #Save today's date in a variable
 TODAY=`date +"%m/%d"`
 
 #Print variable today just to check if it has correct date or no.
-#echo $TODAY
+echo $TODAY
+#Over here you really do not need cat command it is jsut there so that your other command would work.
 cat todaysBdays.txt | grep $TODAY bdayPlusEmail.txt > todaysBdays.txt 
-#awk "{print $2}" todaysBdays.txt > bdaysList.txt 
 #Create file which only has the email address of the people whoever have a birtday today.
+cut -d " " -f2 todaysBdays.txt > sendEmailto.txt
 #The next line changes the mode of the input from default of command line to a file.
 exec < $birthdayList
 
@@ -24,7 +25,6 @@ while read line
 	#Using the things you learned from the youtube video as mentioned in your README.md of this project.
 	ssmtp  $line < bdayWish.txt 2> errorLog.txt  
 done
-
 
 
 
