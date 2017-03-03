f919bfb25f7937123a8ccc30659d59ea12aef159
parths3an <parth.93mukesh@gmail.com>
2017-03-03 01:55:48 -0800

    Final working version of basic idea; Add a user prompt to add info in this formate: 10/14 Parth parth...@gmail.com

--- wishAutomation_Script/wish.sh
+++ wishAutomation_Script/wish.sh
@@ -6,25 +6,40 @@
 #save the input file ina variable to use 
 birthdayList=sendEmailto.txt
 
+#Remove the file wishSentto.txt once you are done with the day, we can not have it at the end.
+#If we delete at the end of this program then we won't be able to see it. 
+rm wishSentto.txt
+
+
 #Save today's date in a variable
 TODAY=`date +"%m/%d"`
 
 #Print variable today just to check if it has correct date or no.
-echo $TODAY
+#Add today's date to show the results.
+echo $TODAY > wishSentto.txt
+
+
 #Over here you really do not need cat command it is jsut there so that your other command would work.
 cat todaysBdays.txt | grep $TODAY bdayPlusEmail.txt > todaysBdays.txt 
 #Create file which only has the email address of the people whoever have a birtday today.
-cut -d " " -f2 todaysBdays.txt > sendEmailto.txt
+cut -d " " -f3 todaysBdays.txt > sendEmailto.txt 
+
+#Create a file which only has the name of the people whoever you sent wishes to.
+cut -d " " -f2 todaysBdays.txt >> wishSentto.txt
+
 #The next line changes the mode of the input from default of command line to a file.
 exec < $birthdayList
 
 #loop through the end of the file loop will terminate by itself when you reach the end of the file.
 while read line
 	do  
-	echo "Email sent to : $line "
+	echo "Email sent to : $line"  
 	#Using the things you learned from the youtube video as mentioned in your README.md of this project.
 	ssmtp  $line < bdayWish.txt 2> errorLog.txt  
 done
+
+#Delete the sendEmailto.txt once you are done running the script for each day
+rm sendEmailto.txt
 
 
 
