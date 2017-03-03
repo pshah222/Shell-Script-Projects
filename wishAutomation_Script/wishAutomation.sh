4bb71bb6a840fcecd730edc0a024f2b0210ef08d
parths3an <parth.93mukesh@gmail.com>
2017-03-02 20:49:05 -0800

    Added the code to read email addresses from the file; It works and saves error to errorLog.txt and created .gitignore to hide the email addresses.

--- wishAutomation_Script/wishAutomation.sh
+++ wishAutomation_Script/wishAutomation.sh
@@ -3,8 +3,16 @@
 
 #Ask user to pass the email address they want to send email.
 #Use the positional parameter to retrive the receiver's email address.
+#save the input file ina variable to use 
+birthdayList=emailList.txt
 
-ssmtp a | emailList.txt < wishBirthday.txt  
+exec < $birthdayList
+while read line
+do  
+echo "Email sent to : $line "
+ssmtp  $line < wishBirthday.txt 2> errorLog.txt  
+done
+
 
 
 
