3204823f1d3ed869c65774de3d0240408786915e
parths3an <parth.93mukesh@gmail.com>
2017-03-04 03:04:38 -0800

    Updated the addPeople.sh; It is complete but need to fix bugs

--- wishAutomation_Script/addPeople.sh
+++ wishAutomation_Script/addPeople.sh
@@ -1,6 +1,19 @@
 #This script will prompt you to provide information of the new people that you want to add in the bdayPlusEmail.txt and later on it will also have option of having a feature to delete some names if you like. 
 
+#############################
+# Global Varialbe Declaration
+#############################
+birthday=""
+nameofBdayPerson=""
+emailAddress=""
+userInputVerification="s"
 
+#########################
+#Function definitions - we have to create function def firrst before using it.
+#########################
+#1. Create a function to ask for user info
+inputPeopleFun()
+{
 echo "Please enter the birtday you want to add in MM/DD formate ony:"
 read birthday
 echo "Enter name of the person:"
@@ -14,16 +27,72 @@ echo "You entered: "
 #The line below displays the user input
 echo "$birthday" "$nameofBdayPerson" "$emailAddress"
 
-echo "Please enter r/R to reenter, q/Q to quit without adding, s/S to save info to the list."
+echo "Please enter r to reenter, q to quit without adding, s to save info to the list."
 read userInputVerification
+}
 
-while [ -z userInputVerification ]
-do
-echo "Please enter one of the above options."
-read userInputVerification
-done
+#2. If there is no input validation enter, recall the script.
+noValidationEntry()
+{
+  #In the case if the user does not enter any input for userInputVerification
+  while [ ${#userInputVerification} -eq  0 ]
+	do 
+	#Keep calling the function until the user doesn't provide some input.
+	scriptExecutionFun
+	break
+  done
+}
 
-if $userInputVerification==r || $userInputVerification==R 
-#The line below only adds a new entry to the list.
-echo $birthday $nameofBdayPerson $emailAddress >> bdayPlusEmail.txt
- 
+#3. If there is some user input for the valdation 
+someValidationEntry()
+{
+  #In the case when user enter some input for verificaiton
+  while [ ${#userInputVerification} -ne  0 ]
+  do 
+	if [ "$userInputVerification" -eq "s" || "$userInputVerification" -eq "S" ] 
+	then
+	   #The line below only adds a new entry to the list.
+           echo $birthday $nameofBdayPerson $emailAddress >> bdayPlusEmail.txt
+	   echo " \"${nameofBdayPerson}\" was added to the list."
+	   break
+	elif [ $userInputVerification -eq "r" || $userInputVerification -eq "R" ]
+	then #If user has some error rerun the whole script.
+	   scriptExecutionFun
+           break 	
+	elif [ $userInputVerification -eq "q" || $userInputVerification -eq "Q" ]
+	then
+	   break
+	else #If user did not enter r,s, or q, restart thw whole script.
+	   scriptExecutionFun
+	   break
+	fi
+  done
+}	
+
+#4. Script execution Fun
+scriptExecutionFun()
+{
+#To get the user input
+inputPeopleFun
+echo "$userInputVerification"
+
+#Valide userInputverification variable.
+noValidationEntry
+
+#Validate if user entered something
+someValidationEntry
+}
+
+
+
+
+#########################
+#Function Calls
+#########################
+scriptExecutionFun
+
+
+
+  
+
+ 	 
